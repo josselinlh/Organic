d8d391be65ec6ddb860879550fe0f5c3ce87b045
josselinlh2 <josselin.lhopitallier@isen-lille.fr>
2015-06-12 18:00:08 +0300

    End of origin
    add css js imgs

---
+++ js/metro.js
@@ -0,0 +1,61 @@
+(function(){
+
+var textMenu = ["Bannière Organic Truck",
+				"L'organic Truck",
+				"Richesse Locale",
+				"Partage des Recettes",
+				"Qui sommes nous?"];
+
+$(document).ready(function(){
+  TweenLite.to($("#caption"),0,{css:{top:0},delay:0, ease:Power2.easeOut});
+  $("#backbutton").hide();
+  //TweenLite.to($("#btn1"),2,{css:{left:0},delay:0, ease:Power2.easeOut});
+  //TweenLite.to($("#btn2"),2,{css:{left:20},delay:0, ease:Power2.easeOut});
+  //TweenLite.to($("#btn3"),2,{css:{left:40},delay:0, ease:Power2.easeOut});
+  //TweenLite.to($("#btn4"),2,{css:{left:0},delay:0.5, ease:Power2.easeOut});
+
+  });
+  
+function clickToButton(i, show)
+{
+   show = typeof show !== 'undefined' ? show : 0;
+
+	if(show==0){$("#backbutton").show("slow");}
+	else{$("#backbutton").hide("slow");}
+	toggleContentAndButtons();
+	$("#caption").fadeTo("slow",0, function(){
+		$(this).text(textMenu[i]);
+		$(this).fadeTo("slow",1);
+
+	});
+	TweenLite.to($("#content"),0.5,{css:{top:show}, ease:Power2.easeOut});
+
+}
+  
+function toggleContentAndButtons()
+{
+	$("#content, #buttonsToContent").slideToggle("slow");
+}
+  $("#btn1").click(function(){
+	clickToButton(1);
+  });
+  
+  $("#btn2").click(function(){
+	clickToButton(2);	 
+  });
+  
+  $("#btn3").click(function(){
+ 	clickToButton(3);
+  });
+  
+  $("#btn4").click(function(){
+	clickToButton(4);
+  
+  });
+
+  
+   $("#backbutton").click(function(){
+		clickToButton(0,500);
+	});   
+	
+})();
