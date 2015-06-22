3660560687c2799d21d55e7f3352f761d71b930d
josselinlh2 <josselin.lhopitallier@isen-lille.fr>
2015-06-22 11:45:45 +0300

    Boutique sans interaction
    Boutique sans interaction

--- js/metro.js
+++ js/metro.js
@@ -6,9 +6,25 @@ var organicMap = ['<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!
                   '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.628702041975!2d3.021058!3d50.634014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c2d55c0d84d577%3A0x8b10cbcee7717a6b!2sEuraTechnologies!5e0!3m2!1sfr!2sfr!4v1434721044176" width="800" height="600" frameborder="0" style="border:0"></iframe>',
                   '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2530.6932507231572!2d3.046525999999992!3d50.632815000000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x941ce6543ee12a17!2sUniversit%C3%A9+Catholique+de+Lille!5e0!3m2!1sfr!2sfr!4v1434721204835" width="800" height="600" frameborder="0" style="border:0"></iframe>'];
 
-var product ="<div class='productGallery'><img class='img-circle img-responsive' src='img/carrots.jpg' /><div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div><div class='productGallery'><img class='img-circle img-responsive' src='img/potatoes.jpg' /><div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div><div class='productGallery'><img class='img-circle img-responsive' src='img/soup.jpg' /><div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div><div class='productGallery'><img class='img-circle img-responsive' src='img/cucumbers.jpg' /><div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>";
+var product ="\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/carrots.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/potatoes.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/soup.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/betterave.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/chou.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/oignons.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/endives.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>\n\
+<div class='productGallery'><img class='img-circle img-responsive' src='img/cucumbers.jpg' />\n\
+<div class='productGalleryToggleButton'><i class='fa fa-cart-arrow-down fa-lg'></i><i class='fa fa-info-circle fa-lg'></i></div></div>";
 
-var Articles = [new ArticleMetro("Où nous trouver?", organicMap[0],0),
+var Articles = [new ArticleMetro("Où trouver l'organic Truck?", organicMap[1],0),
 				new ArticleMetro("Nos produits", product,1),
 				new ArticleMetro("Nos producteurs", "<p>Nos producteurs</p>",2),
 				new ArticleMetro("Partage de nos recettes", "<p>Hello world</p>",3),
